c6e55a474c97dc10f2b367eaaa4ed4ebaba8cd0b
hazem fad <hazemjuninho@yahoo.fr>
2023-02-06 16:40:29 +0100

    weapon+bake+fight

---
+++ Assets/Script/MeleeAttack.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttack : MonoBehaviour
+{
+
+    [SerializeField] int dmg;
+    
+
+        private void OnTriggerEnter(Collider other)
+        {
+        if (other.tag == "Enemy")
+        {
+            Debug.Log(other.transform.gameObject.name);
+            other.GetComponent<EnemyHealth>().TakeDamage(dmg, other.gameObject);
+        }
+            
+        }
+
+    }
+ 
